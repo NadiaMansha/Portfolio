63a5f49ebf716c8a66c2d79ae2733ed57c83892b
Nadia <34932@gcslahore.du.pk>
2023-02-01 12:03:08 +0500

    commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h3>About</h3>
+      <p>
+        I am a student of Computer Science at GCS,Lahore. <br />
+        I have learned HTML,CSS and Vanilla <br /> Java Script and MySqL and
+        Mongodb database. <br />
+        React.js and Node.js
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Interests.jsx
@@ -0,0 +1,12 @@
+export default function  Interests(){
+    return(
+        <div className="interest">
+        <h3>Interests</h3>
+        <p>I am interested in learning new skills such as <br />
+        Full stack web deveopment <br />
+         and Data Science etc.
+
+        </p>
+        </div>
+    )
+}
